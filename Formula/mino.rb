a610990b451ab55226f739a2bfb510ad4ee17262
Dean Sharon <deanshrn@gmain.com>
2026-02-21 22:42:06 +0200

    chore: update Homebrew formula with v1.2.0 SHA256 checksums
    Update all 4 platform checksums from GitHub release artifacts:
    - darwin-x64: 0b0ec7654d975c420cf367b705d85b2d6475a55386975a3812c5dbff2c866aa6
    - darwin-arm64: 5f382007e9d4b7fe0746fb11ef788418636413e9e12e7e5785a99ed0cbbe47b9
    - linux-x64: c73517a994b9ed4b5ad4457f2c8c95a6912faa7c8be2d5e925bde682a89b0c39
    - linux-arm64: 2d4c3ca0ca00494df463c7a6118f4ec94cc5cccc829c0bf03920d9ff482909c9

--- Formula/mino.rb
+++ Formula/mino.rb
@@ -7,24 +7,24 @@ class Mino < Formula
   on_macos do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_X64"
+      sha256 "0b0ec7654d975c420cf367b705d85b2d6475a55386975a3812c5dbff2c866aa6"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "5f382007e9d4b7fe0746fb11ef788418636413e9e12e7e5785a99ed0cbbe47b9"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X64"
+      sha256 "c73517a994b9ed4b5ad4457f2c8c95a6912faa7c8be2d5e925bde682a89b0c39"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "2d4c3ca0ca00494df463c7a6118f4ec94cc5cccc829c0bf03920d9ff482909c9"
     end
   end
 
