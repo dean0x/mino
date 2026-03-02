d68a6e6ef8ce765362fc545ff034b9edf89b496c
Dean Sharon <deanshrn@gmain.com>
2026-03-03 01:11:54 +0200

    chore: update Homebrew formula with v1.2.1 SHA256 checksums

--- Formula/mino.rb
+++ Formula/mino.rb
@@ -1,30 +1,30 @@
 class Mino < Formula
   desc "Secure AI agent sandbox wrapper using Podman rootless containers"
   homepage "https://github.com/dean0x/mino"
-  version "1.2.0"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-apple-darwin.tar.gz"
-      sha256 "0b0ec7654d975c420cf367b705d85b2d6475a55386975a3812c5dbff2c866aa6"
+      sha256 "ed70f21df3f67dc7905ef1158bbbd3d730a632a958e725c8e64ca6531f39aff6"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-apple-darwin.tar.gz"
-      sha256 "5f382007e9d4b7fe0746fb11ef788418636413e9e12e7e5785a99ed0cbbe47b9"
+      sha256 "04646b7ba5e9a02d832652f39cc3cdbc6f4f44cd3d693eece23dad5381d8cb91"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c73517a994b9ed4b5ad4457f2c8c95a6912faa7c8be2d5e925bde682a89b0c39"
+      sha256 "fc8d4fe5c62ce7381e4db1c4a99c06fd3eb1dd60feb3c71f6f36a63c75a24327"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2d4c3ca0ca00494df463c7a6118f4ec94cc5cccc829c0bf03920d9ff482909c9"
+      sha256 "06804c19871c989b7deb7c0dbdbc14823f1960d16d542bf9070715fd0aa5d808"
     end
   end
 
