68063d74e33de2e8aad82d4ebb1f679844dcf345
Dean Sharon <deanshrn@gmain.com>
2026-03-05 14:23:45 +0200

    chore: update Homebrew formula with v1.2.2 SHA256 checksums

--- Formula/mino.rb
+++ Formula/mino.rb
@@ -7,24 +7,24 @@ class Mino < Formula
   on_macos do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_CHECKSUM"
+      sha256 "5aa7296172bc93b3cde45d0b8dccfff916c8284bacc43a41ff53fbe4e7db9a37"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_CHECKSUM"
+      sha256 "8e1514fff695f37dbece21905939dd09952d5032cab9380323d1c977a891e254"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_CHECKSUM"
+      sha256 "acab8b9ed30e7015f3d63b47a177d27e0623ffe2ccd23b89f0b3edf74ec286a7"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_CHECKSUM"
+      sha256 "9578ae0a3161bbfdf06ee1840b5ab927f146f0b5f42dbf7db2a58fa77d9d1d8c"
     end
   end
 
